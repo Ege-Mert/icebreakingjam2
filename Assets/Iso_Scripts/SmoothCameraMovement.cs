1d74e1579a9187d2e4c2415e84037266e96493b6
Egora <egemert669@gmail.com>
2023-12-18 09:13:41 +0300

    Version 1.3 Camera Stops at camera borders and little polish to the game textures some unused content re-Aded

--- Assets/Iso_Scripts/SmoothCameraMovement.cs
+++ Assets/Iso_Scripts/SmoothCameraMovement.cs
@@ -7,14 +7,30 @@ public class SmoothCameraMovement : MonoBehaviour
     public Transform target;
     public Vector3 offset;
     public float damping;
+    public Vector2 minBounds;
+    public Vector2 maxBounds;
 
     private Vector3 velocity = Vector3.zero;
 
     private void FixedUpdate()
     {
-        Vector3 movePosition = target.position + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, damping);
+        void Start()
+        {
+            // Hide the mouse cursor
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        if (target != null)
+        {
+            Vector3 targetPosition = target.position + offset;
+            Vector3 clampedPosition = new Vector3(
+                Mathf.Clamp(targetPosition.x, minBounds.x, maxBounds.x),
+                Mathf.Clamp(targetPosition.y, minBounds.y, maxBounds.y),
+                targetPosition.z
+            );
+
+            transform.position = Vector3.SmoothDamp(transform.position, clampedPosition, ref velocity, damping);
+        }
     }
-
-
 }
